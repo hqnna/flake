5e0fb8de7bc63598b12ff03fd18bd6d84a04df7a
hanna <me@hanna.lol>
2025-03-27 09:59:00 -0400

    refactor: add agenix public key config

---
+++ secrets/secrets.nix
@@ -0,0 +1,6 @@
+let
+	hanna = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEST6MgqRSn0N9ResAQ6Alt0V02GJF3XWneEDeheuQVI";
+	crystal = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKnCWcC9Dinn1nC3uIDKJ/mLTkQ0+jSJQYejyDH8e9iN";
+in {
+  "wireguard.age".publicKeys = [ hanna crystal ];
+}
