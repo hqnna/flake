123ab95162f56dbe987738c1bbbc557eada4624b
hanna <me@hanna.lol>
2025-03-27 09:58:57 -0400

    refactor: change btop theme to onedark

--- users/hanna/btop.nix
+++ users/hanna/btop.nix
@@ -5,8 +5,8 @@
     enable = true;
 
     settings = {
-      color_theme = "TTY";
-      them_background = false;
+      color_theme = "onedark";
+      theme_background = false;
       truecolor = true;
       force_tty = false;
       presets = "cpu:1:default,proc:0:default cpu:0:default,mem:0:default,net:0:default cpu:0:block,net:0:tty";
