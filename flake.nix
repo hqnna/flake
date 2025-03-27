88c257f8c642c9e202edd29553eafcfe42a60849
hanna <me@hanna.lol>
2025-03-27 09:59:00 -0400

    refactor: reorganize and format system flake

--- flake.nix
+++ flake.nix
@@ -10,43 +10,43 @@
     age.inputs.darwin.follows = "";
   };
 
-  outputs = { self, nixpkgs, wsl, hm, age }@inputs: {
-    nixosConfigurations.amethyst = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
+  outputs = { self, nixpkgs, wsl, hm, age }@inputs:
+    let
+      shared = {
+        configs = ./shared/configs;
+        services = ./shared/services;
+      };
+    in {
+      nixosConfigurations = {
+        amethyst = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
 
-      specialArgs = {
-        inherit inputs;
+          specialArgs = {
+            inherit inputs;
+            inherit shared;
+          };
 
-        shared = {
-          configs = ./shared/configs;
-          services = ./shared/services;
+          modules = [
+            ./hosts/amethyst/config.nix
+            wsl.nixosModules.default
+            hm.nixosModules.default
+          ];
+        };
+
+        crystal = nixpkgs.lib.nixosSystem {
+          system = "aarch64-linux";
+
+          specialArgs = {
+            inherit inputs;
+            inherit shared;
+          };
+
+          modules = [
+            ./hosts/crystal/config.nix
+            age.nixosModules.default
+            hm.nixosModules.default
+          ];
         };
       };
-
-      modules = [
-        ./hosts/amethyst/config.nix
-        wsl.nixosModules.default 
-        hm.nixosModules.default
-      ];
     };
-
-    nixosConfigurations.crystal = nixpkgs.lib.nixosSystem {
-      system = "aarch64-linux";
-
-      specialArgs = {
-        inherit inputs;
-
-        shared = {
-          configs = ./shared/configs;
-          services = ./shared/services;
-        };
-      };
-
-      modules = [
-        ./hosts/crystal/config.nix
-        age.nixosModules.default
-        hm.nixosModules.default
-      ];
-    };
-  };
 }
