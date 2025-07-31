0a9488f0282ab023118fb81cf55c4dc83760329f
hanna <me@hanna.lol>
2025-07-30 20:45:31 -0400

    refactor: add `agenix` to flake inputs

--- flake.nix
+++ flake.nix
@@ -1,15 +1,17 @@
 {
   inputs = {
     hjem.url = "github:feel-co/hjem/main";
+    age.url = "github:ryantm/agenix/main";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
     wsl.url = "github:nix-community/nixos-wsl/release-25.05";
     lsr.url = "git+https://tangled.sh/@rockorager.dev/lsr?ref=main";
     hjem.inputs.nixpkgs.follows = "nixpkgs";
+    age.inputs.nixpkgs.follows = "nixpkgs";
     lsr.inputs.nixpkgs.follows = "nixpkgs";
     wsl.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, hjem, wsl, lsr }@inputs: {
+  outputs = { self, nixpkgs, hjem, wsl, lsr, age }@inputs: {
     nixosConfigurations.mercury = nixpkgs.lib.nixosSystem {
       specialArgs = { inherit inputs; };
       system = "x86_64-linux";
@@ -17,6 +19,7 @@
       modules = [
         ./hosts/mercury/config.nix
         hjem.nixosModules.default
+        age.nixosModules.default
         wsl.nixosModules.default
       ];
     };
