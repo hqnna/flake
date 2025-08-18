afe453b0cc740439d0b00c0d1b8cdc4f04483d4b
hanna <me@hanna.lol>
2025-08-18 08:59:51 -0400

    fix: make starship prompt not hideous

--- users/hanna/starship.nix
+++ users/hanna/starship.nix
@@ -8,14 +8,14 @@ toml.generate "starship.toml" {
   add_newline = false;
   scan_timeout = 60;
 
-  format = "$nix_shell$username$hostname$directory$git_branch\${custom.end}";
+  format = "$nix_shell$username$hostname$directory$git_branch[\\]\\$](fg:8) ";
 
   nix_shell = {
-    format = "[\\[](fg:8)[$state](fg:4)[\\[](fg:8)";
+    format = "[\\[](fg:8)[$state](fg:4)[\\]](fg:8)";
   };
 
   username = {
-    format = "[\\[](fg:8)[$user](fg:8)";
+    format = "[\\[](fg:8)[$user](fg:4)";
     show_always = true;
   };
 
@@ -31,9 +31,5 @@ toml.generate "starship.toml" {
 
   git_branch = {
     format = "[@](fg:8)[$branch](fg:3)";
-  };
-
-  custom.end = {
-    format = "[\\]$](fg:8) ";
   };
 }
